c48182ee8c975feed44003a4d74d5213d6095f5d
Stephen Carman <stephen.carman@clarivate.com>
2018-08-24 16:48:13 -0400

    Deleting terms is there, still needs some tests right now

--- src/handlers/index.rs
+++ src/handlers/index.rs
@@ -6,7 +6,9 @@ use std::fs;
 use std::io::Result as IOResult;
 use std::panic::RefUnwindSafe;
 use std::sync::RwLock;
+use std::collections::HashMap;
 
+use hyper::Method;
 use tantivy::schema::*;
 use tantivy::{Document, Index};
 
@@ -21,6 +23,11 @@ macro_rules! add_field {
 #[derive(Deserialize, Debug)]
 pub struct IndexDoc {
     fields: Vec<FieldValues>,
+}
+
+#[derive(Deserialize)]
+pub struct DeleteDoc {
+    terms: HashMap<String, String>,
 }
 
 #[derive(Deserialize, Debug)]
@@ -48,6 +55,37 @@ impl IndexHandler {
         }
     }
 
+    fn delete_request(self, mut state: State, index_path: IndexPath) -> Box<HandlerFuture> {
+        if self.catalog.read().unwrap().exists(&index_path.index) {
+            let f = Body::take_from(&mut state).concat2().then(move |body| match body {
+                Ok(b) => {
+                    let t: DeleteDoc = serde_json::from_slice(&b).unwrap();
+                    {
+                        let index_lock = self.catalog.read().unwrap();
+                        let index = index_lock.get_index(&index_path.index).unwrap();
+                        let index_schema = index.schema();
+                        let mut index_writer = index.writer(SETTINGS.writer_memory).unwrap();
+                        for (field, value) in t.terms {
+                            let f = match index_schema.get_field(&field) {
+                                Some(v) => v,
+                                None => return handle_error(state, &Error::UnknownIndexField(field)),
+                            };
+                            let term = Term::from_field_text(f, &value);
+                            index_writer.delete_term(term);
+                        }
+                        index_writer.commit().unwrap();
+                    }
+                    let resp = create_response(&state, StatusCode::Created, None);
+                    future::ok((state, resp))
+                }
+                Err(ref e) => handle_error(state, e),
+            });
+            Box::new(f)
+        } else {
+            Box::new(handle_error(state, &Error::UnknownIndex(index_path.index)))
+        }
+    }
+
     fn add_to_document(schema: &Schema, field: FieldValues, doc: &mut Document) -> Result<()> {
         match field {
             FieldValues::StrField { field, value } => add_field!(add_text, schema, doc, field, &value),
@@ -61,56 +99,60 @@ impl Handler for IndexHandler {
     fn handle(mut self, mut state: State) -> Box<HandlerFuture> {
         let url_index = IndexPath::try_take_from(&mut state);
         match url_index {
-            Some(ui) => {
-                if self.catalog.read().unwrap().exists(&ui.index) {
-                    let f = Body::take_from(&mut state).concat2().then(move |body| match body {
-                        Ok(b) => {
-                            let t: IndexDoc = serde_json::from_slice(&b).unwrap();
-                            info!("{:?}", t);
-                            {
-                                let index_lock = self.catalog.read().unwrap();
-                                let index = index_lock.get_index(&ui.index).unwrap();
-                                let index_schema = index.schema();
-                                let mut index_writer = index.writer(SETTINGS.writer_memory).unwrap();
-                                let mut doc = Document::new();
-                                for field in t.fields {
-                                    match IndexHandler::add_to_document(&index_schema, field, &mut doc) {
-                                        Ok(_) => {}
-                                        Err(ref e) => return handle_error(state, e),
+            Some(ui) => match *Method::borrow_from(&state) {
+                Method::Delete => self.delete_request(state, ui),
+                Method::Put => {
+                    if self.catalog.read().unwrap().exists(&ui.index) {
+                        let f = Body::take_from(&mut state).concat2().then(move |body| match body {
+                            Ok(b) => {
+                                let t: IndexDoc = serde_json::from_slice(&b).unwrap();
+                                info!("{:?}", t);
+                                {
+                                    let index_lock = self.catalog.read().unwrap();
+                                    let index = index_lock.get_index(&ui.index).unwrap();
+                                    let index_schema = index.schema();
+                                    let mut index_writer = index.writer(SETTINGS.writer_memory).unwrap();
+                                    let mut doc = Document::new();
+                                    for field in t.fields {
+                                        match IndexHandler::add_to_document(&index_schema, field, &mut doc) {
+                                            Ok(_) => {}
+                                            Err(ref e) => return handle_error(state, e),
+                                        }
                                     }
+                                    index_writer.add_document(doc);
+                                    index_writer.commit().unwrap();
                                 }
-                                index_writer.add_document(doc);
-                                index_writer.commit().unwrap();
+                                let resp = create_response(&state, StatusCode::Created, None);
+                                future::ok((state, resp))
                             }
-                            let resp = create_response(&state, StatusCode::Created, None);
-                            future::ok((state, resp))
-                        }
-                        Err(ref e) => handle_error(state, e),
-                    });
-                    Box::new(f)
-                } else {
-                    let f = Body::take_from(&mut state).concat2().then(move |body| match body {
-                        Ok(b) => {
-                            let schema: Schema = match serde_json::from_slice(&b) {
-                                Ok(v) => v,
-                                Err(ref e) => return handle_error(state, e),
-                            };
-                            let mut index_path = self.catalog.read().unwrap().base_path().clone();
-                            index_path.push(&ui.index);
-                            if !index_path.exists() {
-                                fs::create_dir(&index_path).unwrap()
-                            }
-                            let new_index = Index::create_in_dir(index_path, schema).unwrap();
-                            self.add_index(ui.index, new_index);
+                            Err(ref e) => handle_error(state, e),
+                        });
+                        Box::new(f)
+                    } else {
+                        let f = Body::take_from(&mut state).concat2().then(move |body| match body {
+                            Ok(b) => {
+                                let schema: Schema = match serde_json::from_slice(&b) {
+                                    Ok(v) => v,
+                                    Err(ref e) => return handle_error(state, e),
+                                };
+                                let mut index_path = self.catalog.read().unwrap().base_path().clone();
+                                index_path.push(&ui.index);
+                                if !index_path.exists() {
+                                    fs::create_dir(&index_path).unwrap()
+                                }
+                                let new_index = Index::create_in_dir(index_path, schema).unwrap();
+                                self.add_index(ui.index, new_index);
 
-                            let resp = create_response(&state, StatusCode::Created, None);
-                            future::ok((state, resp))
-                        }
-                        Err(ref e) => handle_error(state, e),
-                    });
-                    Box::new(f)
-                }
-            }
+                                let resp = create_response(&state, StatusCode::Created, None);
+                                future::ok((state, resp))
+                            }
+                            Err(ref e) => handle_error(state, e),
+                        });
+                        Box::new(f)
+                    }
+                },
+                _ => unreachable!()
+            },
             None => Box::new(handle_error(state, &Error::UnknownIndex("No valid index in path".to_string()))),
         }
     }
