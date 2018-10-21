c187356ee6c6c6683665e4fe98c4191bd5443f03
Stephen Carman <hntd187@users.noreply.github.com>
2018-10-20 22:50:11 -0400

    Add Consul host properties to settings (#25)
    * Add consul host properties to settings

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -83,7 +83,7 @@ pub fn runner() -> i32 {
         info!("Reading config from: {}", cfg);
         Settings::new(cfg).expect("Invalid Config file")
     } else {
-        Settings::from_args(options)
+        Settings::from_args(&options)
     };
 
     std::env::set_var("RUST_LOG", &settings.log_level);
