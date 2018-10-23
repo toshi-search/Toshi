017e1a8bcc5b1cd11c698e8e22e7f43b5b33750e
Stephen Carman <shcarman@gmail.com>
2018-10-22 21:14:16 -0400

    Update Paths and Permissions

--- ci/install_capnp.sh
+++ ci/install_capnp.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-capnp_verion="0.7.0"
+capnp_version="0.7.0"
 
-curl -O https://capnproto.org/capnproto-c++-${capnp_verion}.tar.gz
-tar zxf capnproto-c++-0.6.1.tar.gz
-cd capnproto-c++-0.6.1
+curl -O "https://capnproto.org/capnproto-c++-${capnp_version}.tar.gz"
+tar zxf capnproto-c++-${capnp_version}.tar.gz
+cd capnproto-c++-${capnp_version}
 ./configure --prefix=$HOME
 make -j6 check
 sudo make install
