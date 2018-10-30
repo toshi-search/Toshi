abb5322297b0782e13f3e4c29b0533ea748bf675
Fletcher Haynes <fletcher@capitalprawn.com>
2018-10-30 14:32:45 -0700

    Added node metadata structs to node.rs

--- src/cluster/node.rs
+++ src/cluster/node.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
@@ -24,4 +25,61 @@ pub fn read_node_id(p: &str) -> Result<String, ClusterError> {
         .read_to_string(&mut contents)
         .map_err(|e| ClusterError::FailedReadingNodeID(e))?;
     Ok(contents)
+}
+
+/// Collection of all the metadata we can gather about the node. It is composed of
+/// sub-structs, listed below. This will be serialized to JSON and sent to Consul.
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Metadata {
+    network: NetworkMetadata,
+    cpu: CPUMetadata,
+    ram: RAMMetadata,
+    disks: Vec<DiskMetadata>,
+    directories: Vec<DirectoryMetadata>,
+}
+
+/// All network data about the node
+#[derive(Debug, Serialize, Deserialize)]
+pub struct NetworkMetadata {
+    ipv4: String,
+    ipv6: String,
+    port: u16,
+}
+
+/// CPU data about the node. Usage is meant to be 0.0-1.0. Not sure how to calculate it yet.
+#[derive(Debug, Serialize, Deserialize)]
+pub struct CPUMetadata {
+    number: HashMap<String, u32>,
+    usage: f32,
+}
+
+/// Metadata about the node's RAM. Units are in GB.
+#[derive(Debug, Serialize, Deserialize)]
+pub struct RAMMetadata {
+    total: u64,
+    free: u64,
+    used: u64,
+}
+
+/// Metadata about the block devices on the node
+#[derive(Debug, Serialize, Deserialize)]
+pub struct DiskMetadata {
+    iowait: f64,
+    disk_type: String,
+    writes_per_second: f64,
+    reads_per_second: f64,
+    write_latency_ms: f64,
+    read_latency_ms: f64,
+    device_path: String,
+}
+
+/// Metadata about the directories on the node
+#[derive(Debug, Serialize, Deserialize)]
+pub struct DirectoryMetadata {
+    device_path: String,
+    directory: String,
+    max_size: u64,
+    current_usage: u64,
+    current_inodes: u64,
+    free_inodes: u64,
 }
