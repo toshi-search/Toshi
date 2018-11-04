c8a11f6ecc01d39cb0a4f21a043c7dad803a3a03
Fletcher Haynes <fletcher@capitalprawn.com>
2018-11-04 15:37:34 -0800

    Added node metadata structs to node.rs (#48)
    Added node metadata structs and functions to collect it.

--- src/cluster/mod.rs
+++ src/cluster/mod.rs
@@ -1,23 +1,39 @@
 //! Contains code related to clustering
 
-use std;
-
 pub mod consul_interface;
 pub mod node;
 
 pub use self::consul_interface::ConsulInterface;
 pub use self::node::*;
 
-#[derive(Debug, Fail)]
+#[derive(Debug, Fail, Serialize, Deserialize)]
 pub enum ClusterError {
     #[fail(display = "Node has no ID")]
     MissingNodeID,
     #[fail(display = "Unable to determine cluster ID")]
     MissingClusterID,
     #[fail(display = "Unable to write node ID: {}", _0)]
-    FailedWritingNodeID(std::io::Error),
+    FailedWritingNodeID(String),
     #[fail(display = "Failed registering Node")]
     FailedRegisteringNode,
     #[fail(display = "Failed reading NodeID: {}", _0)]
-    FailedReadingNodeID(std::io::Error),
+    FailedReadingNodeID(String),
+    #[fail(display = "Unable to retrieve disk metadata: {}", _0)]
+    FailedGettingDirectoryMetadata(String),
+    #[fail(display = "Unable to retrieve block device metadata: {}", _0)]
+    FailedGettingBlockDeviceMetadata(String),
+    #[fail(display = "Unable to find that directory: {}", _0)]
+    NoMatchingDirectoryFound(String),
+    #[fail(display = "Unable to find that block device: {}", _0)]
+    NoMatchingBlockDeviceFound(String),
+    #[fail(display = "Unable to read device RAM information: {}", _0)]
+    FailedGettingRAMMetadata(String),
+    #[fail(display = "Unable to get CPU metadata: {}", _0)]
+    FailedGettingCPUMetadata(String),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum DiskType {
+    SSD,
+    HDD,
 }
