b81c5a2a87a545b0547dc72188df5a7158cc7875
Lucio Franco <luciofranco14@gmail.com>
2018-11-23 00:23:59 -0500

    Rework how the main function spawns futures
    This change adds a shutdown signal and creates its own tokio runtime
    to be able to handle the shutdown.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,40 +1,50 @@
-extern crate gotham;
-extern crate pretty_env_logger;
-extern crate uuid;
-#[macro_use]
-extern crate log;
-#[macro_use]
 extern crate clap;
 extern crate futures;
+extern crate gotham;
 extern crate hyper;
+extern crate log;
 extern crate num_cpus;
+extern crate pretty_env_logger;
 extern crate systemstat;
 extern crate tokio;
 extern crate toshi;
+extern crate uuid;
 
-use futures::Future;
+use clap::{crate_authors, crate_description, crate_version};
+use clap::{App, Arg, ArgMatches};
+use futures::{sync::oneshot, Future, Stream};
+use log::{error, info};
 use std::{
     fs::create_dir,
     path::{Path, PathBuf},
     sync::{Arc, RwLock},
 };
+use tokio::runtime::Runtime;
 use uuid::Uuid;
 
-use toshi::cluster;
-use toshi::cluster::ConsulInterface;
-use toshi::commit::IndexWatcher;
-use toshi::index::IndexCatalog;
-use toshi::router::router_with_catalog;
-use toshi::settings::{Settings, HEADER};
+use toshi::{
+    cluster::{self, ConsulInterface},
+    commit::IndexWatcher,
+    index::IndexCatalog,
+    router::router_with_catalog,
+    settings::{Settings, HEADER},
+};
 
-use clap::{App, Arg, ArgMatches};
+pub fn main() -> Result<(), ()> {
+    let mut rt = Runtime::new().expect("failed to start new Runtime");
 
-pub fn main() {
-    let code = runner();
-    std::process::exit(code);
+    let (tx, shutdown_signal) = oneshot::channel();
+    let server = {
+        let server = runner();
+        server.select(shutdown(tx)).map(|_| ()).map_err(|_| ())
+    };
+
+    rt.spawn(server);
+
+    shutdown_signal.map_err(|_| ()).and_then(move |_| rt.shutdown_now()).wait()
 }
 
-pub fn runner() -> i32 {
+pub fn runner() -> impl Future<Item = (), Error = ()> {
     let options: ArgMatches = App::new("Toshi Search")
         .version(crate_version!())
         .about(crate_description!())
@@ -87,11 +97,6 @@ pub fn runner() -> i32 {
                 .long("cluster-name")
                 .takes_value(true)
                 .default_value("kitsune"),
-        ).arg(
-            Arg::with_name("enable-clustering")
-                .short("E")
-                .long("enable-clustering")
-                .takes_value(false),
         ).get_matches();
 
     let settings = if options.is_present("config") {
@@ -108,41 +113,6 @@ pub fn runner() -> i32 {
     if !Path::new(&settings.path).exists() {
         info!("Base data path {} does not exist, creating it...", settings.path);
         create_dir(settings.path.clone()).expect("Unable to create data directory");
-    }
-
-    if settings.enable_clustering || options.is_present("enable-clustering") {
-        // If this is the first node in a new cluster, we need to register the cluster name in Consul
-        let cluster_name = options.value_of("cluster-name").expect("Unable to get cluster name");
-        let mut consul_client: ConsulInterface = ConsulInterface::default().with_cluster_name(cluster_name.to_string());
-
-        let settings_path_read = settings.path.clone();
-        let settings_path_write = settings.path.clone();
-
-        // Build future that will connect to consul and register the node_id
-        let connect_consul = consul_client
-            .register_cluster()
-            .and_then(move |_| cluster::read_node_id(settings_path_read.as_str()))
-            .then(|result| match result {
-                Ok(id) => {
-                    let parsed_id = Uuid::parse_str(&id).expect("Parsed node ID is not a UUID");
-                    cluster::write_node_id(settings_path_write, parsed_id.to_hyphenated().to_string())
-                }
-
-                Err(_) => {
-                    let new_id = Uuid::new_v4();
-                    cluster::write_node_id(settings_path_write, new_id.to_hyphenated().to_string())
-                }
-            }).and_then(move |id| {
-                consul_client.node_id = Some(id);
-                consul_client.register_node()
-            }).map_err(|err| error!("Error: {}", err));
-
-        // Run the tokio runtime, this will start an event loop that will process
-        // the connect_consul future. It will block until the future is completed
-        // by either completing successfuly or erroring out.
-        tokio::run(connect_consul);
-    } else {
-        info!("Clustering disabled...")
     }
 
     let index_catalog = match IndexCatalog::new(PathBuf::from(&settings.path), settings.clone()) {
@@ -163,6 +133,52 @@ pub fn runner() -> i32 {
 
     println!("{}", HEADER);
 
-    gotham::start(addr, router_with_catalog(&catalog_arc));
-    0
+    // If this is the first node in a new cluster, we need to register the cluster name in Consul
+    let cluster_name = options.value_of("cluster-name").expect("Unable to get cluster name");
+
+    // Run the tokio runtime, this will start an event loop that will process
+    // the connect_consul future. It will block until the future is completed
+    // by either completing successfuly or erroring out.
+    connect_to_consul(settings.path.clone(), cluster_name.into())
+        .and_then(move |_| gotham::init_server(addr, router_with_catalog(&catalog_arc)))
+}
+
+fn connect_to_consul(path: String, cluster_name: String) -> impl Future<Item = (), Error = ()> {
+    let mut consul_client = ConsulInterface::default().with_cluster_name(cluster_name.to_string());
+
+    let settings_path_read = path.clone();
+    let settings_path_write = path.clone();
+
+    // Build future that will connect to consul and register the node_id
+    consul_client
+        .register_cluster()
+        .and_then(move |_| cluster::read_node_id(settings_path_read.as_str()))
+        .then(|result| match result {
+            Ok(id) => {
+                let parsed_id = Uuid::parse_str(&id).expect("Parsed node ID is not a UUID");
+                cluster::write_node_id(settings_path_write, parsed_id.to_hyphenated().to_string())
+            }
+
+            Err(_) => {
+                let new_id = Uuid::new_v4();
+                cluster::write_node_id(settings_path_write, new_id.to_hyphenated().to_string())
+            }
+        }).and_then(move |id| {
+            consul_client.node_id = Some(id);
+            consul_client.register_node()
+        }).map_err(|e| error!("Error: {}", e))
+}
+
+fn shutdown(signal: oneshot::Sender<()>) -> impl Future<Item = (), Error = ()> {
+    // Create an infinite stream of "Ctrl+C" notifications. Each item received
+    // on this stream may represent multiple ctrl-c signals.
+    tokio_signal::ctrl_c()
+        .flatten_stream()
+        .take(1)
+        .into_future()
+        .and_then(move |_| {
+            info!("Gracefully shutting down...");
+            Ok(signal.send(()))
+        }).map(|_| ())
+        .map_err(|_| unreachable!("ctrl-c should never error out"))
 }
