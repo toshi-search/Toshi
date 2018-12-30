017ae4418f177349e960043199228d76a4837b55
Stephen Carman <shcarman@gmail.com>
2018-12-30 17:12:18 -0500

    Fix types and return types thanks to Lucio, I think this is ready for perhaps a test case or so to verify I can get search results via rpc

--- src/cluster/remote_handle.rs
+++ src/cluster/remote_handle.rs
@@ -1,6 +1,5 @@
 //#![allow(dead_code, unused_variables)]
 
-use h2;
 use std::io;
 use std::net::SocketAddr;
 
@@ -8,8 +7,8 @@ use tokio::executor::DefaultExecutor;
 use tokio::net::tcp::ConnectFuture;
 use tokio::net::TcpStream;
 use tokio::prelude::*;
-use tower_grpc::{Error, Request as TowerRequest, Response};
-use tower_h2::client::{Connect, ConnectError, Error as ConnError};
+use tower_grpc::{Error, Request as TowerRequest};
+use tower_h2::client::Connect;
 use tower_http::add_origin::Builder;
 use tower_util::MakeService;
 
@@ -18,8 +17,6 @@ use crate::handle::{IndexHandle, IndexLocation};
 use crate::handlers::index::{AddDocument, DeleteDoc};
 use crate::query::Request;
 use crate::settings::Settings;
-use std::error::Error as StdError;
-use tower_grpc::client::unary::ResponseFuture;
 
 /// A reference to an index stored somewhere else on the cluster, this operates via calling
 /// the remote host and full filling the request via rpc
@@ -31,17 +28,10 @@ pub struct RemoteIndex {
     name: String,
 }
 
-pub enum ServiceError {
-    Conn(ConnectError<io::Error>),
-    H2Conn(ConnError),
-    Grpc(Error),
-}
-
 impl IndexHandle for RemoteIndex {
-    //    type SearchResponse = Box<Future<Item = ResponseFuture<SearchReply, tower_h2::client::ResponseFuture, tower_h2::RecvBody>, Error = ServiceError<Error>> + Send + 'static>;
-    type SearchResponse = Box<ResponseFuture<SearchReply, tower_h2::client::ResponseFuture, tower_h2::RecvBody>>;
-    type DeleteResponse = Box<Future<Item = Response<ResultReply>, Error = Error>>;
-    type AddResponse = Box<Future<Item = Response<ResultReply>, Error = Error>>;
+    type SearchResponse = Box<Future<Item = SearchReply, Error = Error>>;
+    type DeleteResponse = Box<Future<Item = ResultReply, Error = Error>>;
+    type AddResponse = Box<Future<Item = ResultReply, Error = Error>>;
 
     fn get_name(&self) -> String {
         self.name.clone()
@@ -67,10 +57,8 @@ impl IndexHandle for RemoteIndex {
             .and_then(move |mut client| {
                 let bytes = serde_json::to_vec(&search).unwrap();
                 let req = TowerRequest::new(SearchRequest { index: name, query: bytes });
-                Ok(client.search_index(req).map_err(|_| Error::Inner(())))
+                client.search_index(req).map(|res| res.into_inner()).map_err(|_| Error::Inner(()))
             });
-//            .map(|rf| rf.into());
-
 
         Box::new(conn_task)
     }
--- src/cluster/rpc_server.rs
+++ src/cluster/rpc_server.rs
@@ -1,3 +1,62 @@
-use crate::cluster::cluster_rpc::server::*;
+use std::sync::Arc;
+use std::sync::RwLock;
 
-pub struct RpcServer {}
+use tokio::prelude::*;
+use tower_grpc::{Code, Error, Request, Response, Status};
+
+use crate::cluster::cluster_rpc::*;
+use crate::cluster::cluster_rpc::server;
+use crate::handle::IndexHandle;
+use crate::index::IndexCatalog;
+use crate::query;
+
+
+/// RPC Services should "ideally" work on only local indexes, they shouldn't be responsible for
+/// going to other nodes to get index data. It should be the master's duty to know where the local
+/// indexes are stored and make the RPC query to the node to get the data.
+#[derive(Clone)]
+pub struct RpcServer {
+    catalog: Arc<RwLock<IndexCatalog>>,
+}
+
+impl server::IndexService for RpcServer {
+    type PlaceIndexFuture = Box<Future<Item = Response<ResultReply>, Error = Error> + Send + 'static>;
+    type PlaceDocumentFuture = Box<Future<Item = Response<ResultReply>, Error = Error> + Send + 'static>;
+    type PlaceReplicaFuture = Box<Future<Item = Response<ResultReply>, Error = Error> + Send + 'static>;
+    type SearchIndexFuture = Box<Future<Item = Response<SearchReply>, Error = Error> + Send + 'static>;
+
+    fn place_index(&mut self, request: Request<PlaceRequest>) -> Self::PlaceIndexFuture {
+        unimplemented!()
+    }
+
+    fn place_document(&mut self, request: Request<DocumentRequest>) -> Self::PlaceDocumentFuture {
+        unimplemented!()
+    }
+
+    fn place_replica(&mut self, request: Request<ReplicaRequest>) -> Self::PlaceReplicaFuture {
+        unimplemented!()
+    }
+
+    fn search_index(&mut self, request: Request<SearchRequest>) -> Self::SearchIndexFuture {
+        let inner = request.into_inner();
+        if let Ok(cat) = self.catalog.read() {
+            let index = cat.get_index(&inner.index).unwrap();
+            let query: query::Request = serde_json::from_slice(&inner.query).unwrap();
+            let query_results = index.search_index(query).unwrap();
+            let query_bytes: Vec<u8> = serde_json::to_vec(&query_results).unwrap();
+            let resp = Response::new(SearchReply {
+                result: Some(ResultReply {
+                    code: 0,
+                    message: "".into(),
+                }),
+                doc: query_bytes,
+            });
+            Box::new(future::finished(resp))
+        } else {
+            Box::new(future::failed(Error::Grpc(Status::with_code_and_message(
+                Code::NotFound,
+                format!("Index: {} not found", inner.index),
+            ))))
+        }
+    }
+}
