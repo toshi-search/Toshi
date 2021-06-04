6bbbd6de5db53190ec4c90aee18cc935b8ddf340
Stephen Carman <shcarman@gmail.com>
2021-06-04 17:27:08 -0400

    More dep updates, fixed a few warnings

--- toshi-raft/src/raft_io.rs
+++ toshi-raft/src/raft_io.rs
@@ -1,8 +1,8 @@
 use std::net::SocketAddr;
 
 use dashmap::DashMap;
-use message_io::events::EventQueue;
-use message_io::network::{Endpoint, NetEvent, Network, Transport};
+use message_io::events::*;
+use message_io::network::{split as net_split, Endpoint, NetEvent, NetworkController, NetworkProcessor, Transport};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -11,50 +11,65 @@ pub enum RaftEvents {
 }
 
 pub struct RaftIO {
-    net: Network,
-    events: EventQueue<NetEvent>,
+    sender: EventSender<RaftEvents>,
+    receiver: EventReceiver<RaftEvents>,
+    controller: NetworkController,
+    processor: NetworkProcessor,
     peers: DashMap<u32, Endpoint>,
 }
 
 impl Default for RaftIO {
     fn default() -> Self {
-        let (events, net) = Network::split();
+        let (sender, receiver) = split();
+        let (controller, processor) = net_split();
         Self {
-            net,
-            events,
+            sender,
+            receiver,
             peers: DashMap::default(),
+            controller,
+            processor,
         }
     }
 }
 
 impl RaftIO {
     pub fn new(id: u32, ep: Endpoint) -> Self {
-        let (events, net) = Network::split();
+        let (sender, receiver) = split();
+        let (controller, processor) = net_split();
+
         let peers = DashMap::<u32, Endpoint>::new();
         peers.insert(id, ep);
-        Self { net, events, peers }
+        Self {
+            sender,
+            receiver,
+            controller,
+            processor,
+            peers,
+        }
     }
 
     fn join_cluster(&mut self, id: u32, addr: SocketAddr) {
-        if let Ok((endpoint, _)) = self.net.connect(Transport::Tcp, addr) {
+        if let Ok((endpoint, _)) = self.controller.connect(Transport::Tcp, addr) {
             self.peers.insert(id, endpoint);
         }
     }
 
-    pub fn run(mut self, endpoint: String) -> Result<(), Box<dyn std::error::Error>> {
-        self.net.listen(Transport::Tcp, &endpoint)?;
+    pub fn run(self, endpoint: String) -> Result<(), Box<dyn std::error::Error>> {
+        let (sock, _) = self.controller.listen(Transport::Tcp, &endpoint)?;
 
-        loop {
-            match self.events.receive() {
-                NetEvent::Message(ep, payload) => {
-                    let msg = bincode::deserialize::<RaftEvents>(&payload)?;
-                    match msg {
-                        RaftEvents::JoinCluster(id) => self.join_cluster(id, ep.addr()),
-                    }
+        Ok(())
+    }
+    fn process_event(&mut self, msg: NetEvent) {
+        match msg {
+            NetEvent::Message(ep, payload) => {
+                let msg = bincode::deserialize::<RaftEvents>(&payload).unwrap();
+                match msg {
+                    RaftEvents::JoinCluster(id) => self.join_cluster(id, ep.addr()),
                 }
-                NetEvent::Connected(_) => {}
-                NetEvent::Disconnected(_) => {}
             }
+            NetEvent::Connected(_, _) => {}
+            NetEvent::Disconnected(_) => {}
+            NetEvent::Accepted(_, _) => {}
         }
     }
 }
